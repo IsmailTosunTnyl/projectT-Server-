489c0c1582fa7d969d4161731f468e28bc04075a
itosun <ismailtosuntnyl@gmail.com>
2022-11-26 22:06:47 +0300

    forget

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from DataBase import DB
 app = Flask(__name__)
 api = Api(app)
 db = DB()
+
+# User SignUp
 class Signup(Resource):
     def get(self):
         return {"data": ""}
@@ -16,20 +18,37 @@ class Signup(Resource):
             return "ok" , 201
         except:
             return "error" , 500
-        
+
+#User Login      
 class Login(Resource):
     def get(self):
         return {"data": ""}
     
     def post(self,username,password):
-        res = db.login(username,password)
-        if res:
-            return "ok" , 200
-        else:
-            return "error" , 500 #check if user exist in db
-       
+        try:
+            res = db.login(username,password)
+            if res:
+                return "ok" , 200
+            else:
+                return "error" , 500 #check if user exist in db
+        except:
+            return "error" , 404
+
+class Cargoo(Resource):
+    def get(self):
+        return {"data": ""}
+    
+    def post(self,ownerID,):
+        
+
+
+
 
 api.add_resource(Signup, "/signup/<string:firstname>/<string:lastname>/<string:password>/<string:email>/<string:address>/<string:phone>/<string:nationalID>")
 api.add_resource(Login, "/login/<string:username>/<string:password>")
+
+
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0",port=80,debug=True)
